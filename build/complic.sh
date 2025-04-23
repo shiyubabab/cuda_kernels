e5656a7c04bd05e8bad956fb2cc70e3e2bcccbe9
shiyubabab <745371486@qq.com>
2025-04-23 15:55:37 +0800

    update file struct of code and data

---
+++ build/complic.sh
@@ -0,0 +1,10 @@
+##########################################################################
+# File Name: complic.sh
+# Author: mlxh
+# mail: mlxh_gto@163.com
+# Created Time: Wed 23 Apr 2025 03:51:00 PM CST
+#########################################################################
+#!/bin/bash
+PATH=/home/edison/bin:/home/edison/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/work/tools/gcc-3.4.5-glibc-2.3.6/bin
+export PATH
+
---
+++ examples/first.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: first.cpp
+	> Author: mlxh
+	> Mail: mlxh_gto@163.com 
+	> Created Time: Wed 23 Apr 2025 03:53:20 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(void){
+	return 0;
+}
