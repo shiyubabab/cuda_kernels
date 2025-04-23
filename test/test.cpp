a49559a66be9e8c51e011443f4793f7e7ebc61f5
shiyubabab <745371486@qq.com>
2025-04-23 15:50:30 +0800

    update file struct of code and data

---
+++ test/test.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: mlxh
+	> Mail: mlxh_gto@163.com 
+	> Created Time: Wed 23 Apr 2025 03:49:59 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+
+int main(void){
+	return 0;
+}
