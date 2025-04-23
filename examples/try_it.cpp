f34752f74d8b5d4a61eb342954b210d1f8b4fd2f
shiyubabab <745371486@qq.com>
2025-04-23 16:04:18 +0800

    update file struct of code and data.

---
+++ examples/try_it.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: try_it.cpp
+	> Author: mlxh
+	> Mail: mlxh_gto@163.com 
+	> Created Time: Wed 23 Apr 2025 04:03:51 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(void){
+	return 0;
+}
