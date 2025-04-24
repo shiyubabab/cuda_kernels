8a3da167d17bc6559c1643747c9c746d2a8761e0
shiyubabab <745371486@qq.com>
2025-04-24 17:41:49 +0800

    add vector add function code 01.

--- kernels/vector_add.cu
+++ kernels/vector_add.cu
@@ -12,7 +12,9 @@ __global__ void vec_add(fsize *x,fsize *y,fsize *z,int N){
 
 __global__ void vec_add_float4(float *A,float *B,float *C){
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	for(int i = idx;i<MEMORY_OFFSET/4;i+=blockDim.x * gridDim.x){
+	int num_float4 = MEMORY_OFFSET / 4;
+	int num_remaining_floats = MEMORY_OFFSET % 4;
+	for(int i = idx;i<num_float4;i+=blockDim.x * gridDim.x){
 		float4 a1 = reinterpret_cast<float4*>(A)[i];
 		float4 b1 = reinterpret_cast<float4*>(B)[i];
 		float4 c1;
@@ -24,6 +26,12 @@ __global__ void vec_add_float4(float *A,float *B,float *C){
 
 		reinterpret_cast<float4>(C)[i] = c1;
 	}
+
+	if(idx < num_remaining_floats){
+		int remaining_start_index = num_float4 * 4;
+		C[remaining_start_index + idx] = A[remaining_start_index + idx] + B[remaining_start_index + idx];
+	}
+
 }
 
 void vec_add_cpu(fsize *x,fsize *y,fsize *z,int N){
